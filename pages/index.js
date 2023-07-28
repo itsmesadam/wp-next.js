cd038aef910d248f328d2751b1afdbee3cf894fc
saddam andhika <117705270+itsmesadam@users.noreply.github.com>
2023-07-28 13:36:51 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
 import { getAllPosts } from '../lib/test-data';
+import { client } from '../lib/apollo';
+import { gql } from "@apollo/client"
 
 
 export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
-        <title>Headless WP Next Starter</title>
+        <title>WP - Next</title>
         <link rel="icon" href="favicon.ico"></link>
       </Head>
 
@@ -39,7 +41,21 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(){
 
-  const response = await getAllPosts()
+  const GET_POST = gql`
+  query GetAllPosts {
+    posts {
+      nodes {
+        title
+        content
+        uri
+        date
+      }
+    }
+  }
+  `
+  const response = await client.query({
+    query: GET_POST
+  })
   const posts = response?.data?.posts?.nodes
   return {
     props: {
